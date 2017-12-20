4ddbc24369cdcb726cacbf58b7d45fd349a15fcf
gaozx <gaozx@gaozxdeMac-mini.local>
2017-12-20 16:29:15 +0800

    显示新闻内容

--- src/pages/newsdetail/newsdetail.ts
+++ src/pages/newsdetail/newsdetail.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+//import { DomSanitizer } from '@angular/platform-browser/src/security/dom_sanitization_service';
 /**
  * Generated class for the NewsdetailPage page.
  *
@@ -16,12 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class NewsdetailPage {
   strHtml = 'aaaaaaa';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+//  constructor(private sanitizer: DomSanitizer,public navCtrl: NavController, 
+  constructor(public navCtrl: NavController, 
+      public navParams: NavParams) {
     this.strHtml = this.navParams.get('html');
+    this.strHtml = this.strHtml.replace('/drupal/sites/default/files/inline-images/', 
+        'http://192.168.50.80:81/drupal/sites/default/files/inline-images/');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewsdetailPage');
+//    history.replaceState({}, '', "http://192.168.50.80:81")
+//    alert("aa");
+//    window.location.href="http://192.168.50.80:81/drupal" 
+//    this.strHtml.replace('/drupal/sites/default/files/inline-images', 
+//        'http://92.168.50.80:81/drupal/sites/default/files/inline-images/')
+}
+
+/*
+  assembleHTML(strHTML:any) {
+    return this.sanitizer.bypassSecurityTrustHtml(strHTML);
   }
+*/
+//  sanitizeHtml = this.sanitizer.bypassSecurityTrustHtml(this.strHtml);
 
 }
